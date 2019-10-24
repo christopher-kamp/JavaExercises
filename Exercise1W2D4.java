0a7eb77e13c4bbad7be3f3e7b138e97a3e48261a
christopher-kamp <56821044+christopher-kamp@users.noreply.github.com>
2019-10-24 22:13:45 +0300

    Add files via upload

---
+++ Exercise1W2D4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise1w2d4;
+
+/**
+ *
+ * @author chris
+ */
+public class Exercise1W2D4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        School school1 = new School();
+        char grade = school1.grade;
+        int students = school1.students;
+        System.out.println(grade);
+        System.out.println(students);
+    }
+    
+}
---
+++ School.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise1w2d4;
+
+/**
+ *
+ * @author chris
+ */
+public class School {
+    int students;
+    char grade;
+    
+}
